d54bd915e48f10297d76a1e88cfd08087215e92c
Aaron <imaaronacs@gmail.com>
2021-01-12 19:40:24 -0800

    first commit

---
+++ button.js
@@ -0,0 +1,11 @@
+function changeColor() {
+  var blueDiv = document.getElementById("changeable");
+  var backgroundChange = document.getElementById("changeBackground");
+  blueDiv.style.backgroundColor = "red";
+  backgroundChange.style.backgroundColor="purple";
+}
+
+function changeColorBack() {
+  var changeableDiv = document.getElementById("changeable")
+  changeableDiv.style.backgroundColor = 'blue'
+}
